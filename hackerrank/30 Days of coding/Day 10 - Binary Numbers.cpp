f1f4e08cd8636a1f66d7aa0e079557799fa63bc7
Azizur Rahaman <frazizur.rahaman@gmail.com>
2023-05-26 10:35:05 +0600

    new problem solved

---
+++ hackerrank/30 Days of coding/Day 10 - Binary Numbers.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    cin.ignore();
+    
+    string binary="";
+    int counter;
+    int temp;
+    bool counterInitialize=false;
+    
+    //convert n to binary number system
+    for (int i=0; n>0; i++) {
+        binary += to_string(n%2);
+        n=n/2;
+    }
+    
+    
+    for (int i=0; i<binary.length(); i++) {
+      
+      if(binary[i]=='1'){
+          
+          if(counterInitialize==false) {
+              counter++;
+          }
+          else {
+              temp++;
+          }
+      }else{
+          if(counter>0 && counterInitialize==false){
+               counterInitialize=true;
+          }
+          if(temp>counter){
+              counter=temp;
+          }
+      }
+      temp=0;
+        
+    }
+    cout<<counter<<endl;
+    return 0;
+}
