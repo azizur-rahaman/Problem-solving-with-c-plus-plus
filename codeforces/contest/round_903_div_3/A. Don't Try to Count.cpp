a1857209a706c89e6b09351c93bf03283cd7121f
Azizur Rahaman <frazizur.rahaman@gmail.com>
2023-10-22 19:17:38 +0600

    new problem solved

---
+++ codeforces/contest/round_903_div_3/A. Don't Try to Count.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        int x,m,xt,xm, i=1;
+        cin>>x>>m;
+
+        xt=x;
+        xm=m;
+        while (xt--)
+        {
+            char temp;
+            cin>>temp;
+        }
+
+        while (xm--)
+        {
+            char temp;
+            cin>>temp;
+        }
+
+        
+        
+    }
+    
+    
+}
