23cbc4d679846addb58fff0dfe6ee0c5911f0593
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-04-01 19:51:16 +0600

    practive cf

---
+++ contest/codeforeces/div4/Round 937/t.cpp
@@ -0,0 +1,114 @@
+1
+a a
+2
+a a
+a a
+2
+b a
+a a
+2
+a a
+b a
+2
+b a
+b a
+2
+a b
+a a
+2
+b b
+a a
+2
+a b
+b a
+2
+b b
+b a
+2
+a a
+a b
+2
+b a
+a b
+2
+a a
+b b
+2
+b a
+b b
+2
+a b
+a b
+2
+b b
+a b
+2
+a b
+b b
+2
+b b
+b b
+3
+a a
+a a
+a a
+3
+b a
+a a
+a a
+3
+c a
+a a
+a a
+3
+a a
+b a
+a a
+3
+b a
+b a
+a a
+3
+c a
+b a
+a a
+3
+a a
+c a
+a a
+3
+b a
+c a
+a a
+3
+c a
+c a
+a a
+3
+a a
+a a
+b a
+3
+b a
+a a
+b a
+3
+c a
+a a
+b a
+3
+a a
+b a
+b a
+3
+b a
+b a
+b a
+3
+c a
+b a
+b a
+3
+a a
+c a
+b a
